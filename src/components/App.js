e8d17a945e6c3bbb0be6a118ded23db7f8a8b9e0
Joy <joylees@seas.upenn.edu>
2020-10-29 04:45:34 +0700

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => (
+  <div>
+    <h1 style={{ textAlign: 'center' }}>
+      <span role="img" aria-label="celebration">🎉</span>
+      CIS 197 Community
+      <span role="img" aria-label="celebration">🎉</span>
+    </h1>
+  </div>
+
+)
+export default App
