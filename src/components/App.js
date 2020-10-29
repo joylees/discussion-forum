b35db2433af77491a9c1ba3974c51c90d236922b
Joy <joylees@seas.upenn.edu>
2020-10-30 00:20:24 +0700

    fixed styling

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import NewPost from './NewPost'
 
 const App = () => (
   <div style={{ padding: '2rem 20rem 0rem 20rem' }}>
-    <h1 style={{ fontSize: '44px', textAlign: 'center', paddingBottom: '2rem' }}>
+    <h1 style={{ fontSize: '44px', textAlign: 'center', paddingBottom: '0.5rem' }}>
       <span role="img" aria-label="celebration">🎉</span>
       CIS 197 Community
       <span role="img" aria-label="celebration">🎉</span>
